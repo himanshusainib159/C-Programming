1b15268f8a5f7bcedbf869acf968010baadf966f
Himanshu Saini <himanshusainib.159@gmail.com>
2021-08-07 19:07:31 +0530

    init

---
+++ positvenegative.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    //check if number is +ve or -ve 
+    int num;
+    scanf("%d",&num);
+    if(num>=0)
+    {
+        printf("positive number ");
+    }
+    else
+    printf("negative number");
+    return 0;
+}
