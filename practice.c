97dbfe99d610837b27d981d86a2bd689d220b220
Himanshu Saini <himanshusainib.159@gmail.com>
2021-08-09 14:12:39 +0530

    init

--- practice.c
+++ practice.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int c,num;
+    /*int c,num;
     c=1;
     printf("Enter number - ");
     scanf("%d",&num);
@@ -9,6 +9,11 @@ int main()
     {
         printf("%d * %d = %d\n",num,c,num*c);
         c++;
+    }*/
+    int i;
+   // printf("enter number - ");
+    for(i=1;i<=5;i+=1)
+    {
+        printf("%d\n",i*i);
     }
-
 }
