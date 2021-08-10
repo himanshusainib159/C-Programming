68ef3a212923c386689ec8db4272ba1ec19a80b4
Himanshu Saini <himanshusainib.159@gmail.com>
2021-08-10 22:38:41 +0530

    init

--- array.c
+++ array.c
@@ -2,13 +2,8 @@
 int main()
 {
     int A[5];//5 elements (integers) x 2 byte= 10 bytes
-<<<<<<< HEAD
     A[0] =10;
-    A[1] =16;
-=======
-    A[0] =11;
     A[1] =15;
->>>>>>> e645d6d3e08e159e77469bc7cd4bd090f64d8a27
     A[2] =20;
     printf("%d",A[1]);
 }
