b08bff843afa4c0b830ca822d3aa8c6543eab159
Himanshu Saini <himanshusainib.159@gmail.com>
2021-08-11 13:32:58 +0530

    init

--- practice.c
+++ practice.c
@@ -1,19 +1,57 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+  	// Complete the code.
+int main() 
 {
-    /*int c,num;
-    c=1;
-    printf("Enter number - ");
-    scanf("%d",&num);
-    while(c<=10)
+    int a=8, b=11,i;
+    //scanf("%d\n%d",&a,&b);
+    for(i=a;i<=b;i++)
     {
-        printf("%d * %d = %d\n",num,c,num*c);
-        c++;
-    }*/
-    int i;
-   // printf("enter number - ");
-    for(i=1;i<=5;i+=1)
-    {
-        printf("%d\n",i*i);
+      if (i<=9)
+      {
+        switch (i) {
+        case 0:
+          printf("zero\n");
+          break;
+        case 1:
+          printf("one\n");
+          break;
+        case 2:
+          printf("two\n");
+          break;
+        case 3:
+          printf("three\n");
+          break;
+        case 4:
+          printf("four\n");
+          break;
+        case 5:
+          printf("five\n");
+          break;
+        case 6:
+          printf("six\n");
+          break;
+        case 7:
+          printf("seven\n");
+          break;
+        case 8:
+          printf("eight\n");
+          break;
+        case 9:
+          printf("nine\n");
+          break;
+        }
+      } else 
+      {
+        if (i % 2 == 0)
+          printf("even\n");
+        else
+          printf("odd\n");
+      }
     }
+    return 0;
 }
+
